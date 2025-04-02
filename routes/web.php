557b5984612baa848a63f717ed9dd0d1e55d8837
Yaacob Garcia <yaacob@live.com.mx>
2025-04-01 19:02:56 -0600

    Agregar componentes de Blade y configuración de Alpine.js; actualizar composer.json y tailwind.config.js

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GenderController;
-use App\Http\Controllers\UniverseController;
-use App\Http\Controllers\SuperheroController;
 
 Route::get('/', function () {
-
-
     return view('welcome');
 });
-    
 
-Route::resource('universe', UniverseController::class);
-Route::resource('superhero', SuperheroController::class);
-Route::resource('gender', GenderController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
